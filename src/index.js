1dd2d4564e0d02820f9d2238cbf6e4ce3d03e676
Pavel <ppparfenovich@gmail.com>
2021-02-25 18:51:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  let newArr = [0];
+  
+  let arr = newArr.concat(array);
+
+
+
+  // if (array === undefined) {
+  //   return 0;
+  // } else if (array === []) {
+  //   return 0;
+  // }
+
+  let minNumber = Math.min(...arr);
+  return minNumber
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  } else if (array === []) {
+    return 0;
+  }
+
+  let maxNumber = Math.max(...array);
+  return maxNumber
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  } else if (array === []) {
+    return 0;
+  }
+  
+  let sum = 0;
+  let count = 0;
+
+  array.forEach(function (item) {
+    sum += item;
+    count++;
+  });
+  
+  return sum / count;
 }
