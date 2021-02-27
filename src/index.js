b8efa9f1bda840eeb90a79cdb84418560f889ba6
Pavel <ppparfenovich@gmail.com>
2021-02-27 15:48:31 +0300

    fix: solution

--- src/index.js
+++ src/index.js
@@ -1,46 +1,39 @@
 
 exports.min = function min (array) {
-  let newArr = [0];
-  
-  let arr = newArr.concat(array);
+  let arr = [].concat(array);
 
-
-
-  // if (array === undefined) {
-  //   return 0;
-  // } else if (array === []) {
-  //   return 0;
-  // }
-
-  let minNumber = Math.min(...arr);
-  return minNumber
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let minNumber = Math.min(...arr);
+    return minNumber
+  }
 }
 
 exports.max = function max (array) {
-  if (array === undefined) {
-    return 0;
-  } else if (array === []) {
-    return 0;
-  }
+  let arr = [].concat(array);
 
-  let maxNumber = Math.max(...array);
-  return maxNumber
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let maxNumber = Math.max(...arr);
+    return maxNumber
+  }
 }
 
 exports.avg = function avg (array) {
-  if (array === undefined) {
-    return 0;
-  } else if (array === []) {
-    return 0;
-  }
-  
-  let sum = 0;
-  let count = 0;
 
-  array.forEach(function (item) {
-    sum += item;
-    count++;
-  });
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    let count = 0;
   
-  return sum / count;
+    array.forEach(function (item) {
+      sum += item;
+      count++;
+    });
+    
+    return sum / count;
+  }
 }
